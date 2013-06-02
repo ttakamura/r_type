e5123271d34612b69898ca1a13ae0d0ae00078fd
Tatsuya Takamura <takamura@tonchidot.com>
2013-06-02 22:21:56 +0900

    Add sample script

---
+++ sample/sample1.rb
@@ -0,0 +1,34 @@
+require '../lib/r_type'
+
+R.run do
+  print (cos(0) - sqrt(2)) / exp(4)
+
+  x = (1..5).to_a
+  print sqrt(x)
+  print log(x)
+
+  self.x2 = [1, 2, 3, 4, 5, 3]
+  self.y2 = [1, 3, 2, 4, 5, 3]
+  print summary(x2)
+  print cor(x2, y2)
+  print diff(y2)
+  print cumsum(x2)
+
+  print x2 + y2
+  print x2 - y2
+  print x2 + 10
+  print x2 * y2
+end
+# >> [1] -0.007586586
+# >> [1] 1.000000 1.414214 1.732051 2.000000 2.236068
+# >> [1] 0.0000000 0.6931472 1.0986123 1.3862944 1.6094379
+# >>    Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# >>    1.00    2.25    3.00    3.00    3.75    5.00
+# >> [1] 0.9
+# >> [1]  2 -1  2  1 -2
+# >> [1]  1  3  6 10 15 18
+# >> [1]  2  5  5  8 10  6
+# >> [1]  0 -1  1  0  0  0
+# >> [1] 11 12 13 14 15 13
+# >>      [,1]
+# >> [1,]   63
---
+++ sample/sample2.rb
@@ -0,0 +1,38 @@
+require '../lib/r_type'
+
+R.run do
+  x = (0..4).to_a
+
+  double = eval_R <<-RSRC
+    function (x) x * 2
+  RSRC
+
+  print double(10)
+  print sapply(x, double)
+  pow = `function(x) x ^ 2`
+  print sapply(x, pow)
+
+  f = `function(x) exp(x) - 2`
+  print uniroot(f, [0, 1])
+
+  print polyroot([2, 3, 1])
+  print integrate(pow, 0, 1)
+end
+# >>  [1] 0 0 0 0 0 0 0 0 0 0
+# >> [1] 0 2 4 6 8
+# >> [1]  0  1  4  9 16
+# >> $root
+# >> [1] 0.6931457
+# >>
+# >> $f.root
+# >> [1] -2.943424e-06
+# >>
+# >> $iter
+# >> [1] 5
+# >>
+# >> $estim.prec
+# >> [1] 6.103516e-05
+# >>
+# >> [1] -1+0i -2-0i
+# >> 0.3333333 with absolute error < 3.7e-15
+# >> 0.9772499 with absolute error < 1.6e-06
